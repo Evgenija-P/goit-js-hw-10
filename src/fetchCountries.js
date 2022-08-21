8e40f33f34922639431149826790c3d93451e347
Evgenija-P <e.a.poduzova@gmail.com>
2022-08-21 18:54:21 +0300

    if + Error

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,5 +2,10 @@ export function fetchCountries(name) {
   const url = `https://restcountries.com/v3.1/name`;
   return fetch(
     `${url}/${name}?fields=name,capital,population,flags,languages`
-  ).then(response => response.json());
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
 }
